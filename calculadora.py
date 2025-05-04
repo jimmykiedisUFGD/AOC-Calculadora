fc10edfa8bc038eb9a33f6cbbc76bdd273a44b72
jimmykiedis <jinkiedis@icloud.com>
2025-05-03 22:03:12 -0400

    Version 0.0.4

--- calculadora.py
+++ calculadora.py
@@ -5,19 +5,22 @@ def menu_principal(stdscr):
     curses.curs_set(1)
 
     # Exibe o menu principal usando curses e lê a escolha do usuário
-    stdscr.addstr(0, 0, "Calculadora Binária")
-    stdscr.addstr(1, 0, "Operações disponíveis:")
-    stdscr.addstr(2, 2, "Escolha a quantidade de bits:")
-    stdscr.addstr(3, 2, "+ : Adição")
-    stdscr.addstr(4, 2, "- : Subtração")
-    stdscr.addstr(5, 2, "* : Multiplicação")
-    stdscr.addstr(6, 2, "/ : Divisão")
+
+    linha = 0
+    stdscr.addstr(linha, 0, "*--**--Calculadora Binária--**--*")
+ 
+    stdscr.addstr(linha+1, 2, "Escolha a quantidade de bits:")
+    quantidade_bits = escolher_bits(stdscr)
+
+    limpar_tela(stdscr)
+    stdscr.addstr(linha+1, 2, "Operações disponíveis:")
+    stdscr.addstr(linha+2, 3, "+ : Adição")
+    stdscr.addstr(linha+3, 3, "- : Subtração")
+    stdscr.addstr(linha+4, 3, "* : Multiplicação")
+    stdscr.addstr(linha+5, 3, "/ : Divisão")
 
     # Ecreve o menu na tela
     stdscr.refresh()
-
-    # Pergunta quantos bits terá a operação
-    quantidade_bits = escolher_bits(stdscr)
 
     # Lê quais serão os operandos e qual será o operador
     num1, op, num2 = ler_operandos_decimal(stdscr)
@@ -38,44 +41,60 @@ def menu_principal(stdscr):
         erro = 'Entrada inválida'
         mostrar_erro(erro, stdscr)
 
+def mostrar_erro(erro, linha, stdscr):
+    limpar_tela(stdscr)
+    stdscr.addstr(linha-1, 0, f"Erro: {erro}")
+    stdscr.refresh()
+    
+    return
+
 def limpar_tela(stdscr):
 
     # Limpa a tela e exibe o menu principal novamente
     stdscr.clear()
     stdscr.refresh()
-    menu_principal(stdscr)
+    
+    return
     
 def escolher_bits(stdscr):
 
+    linha = 2
     # Definindo se serão 8, 16, 32 bits
-    stdscr.addstr(7, 2, "8, 16, 32:")
+    stdscr.addstr(linha, 3, "8, 16, 32:")
     curses.echo()
-    bits = stdscr.getstr(7, 27, 20).decode("utf-8")
+    bits = stdscr.getstr(linha, len('8, 16, 32: ')+2, 2).decode("utf-8")
     
+    try:
+        bits = int(bits)
+        if bits not in [8, 16, 32]:
+            raise ValueError
+    except ValueError:
+        erro = 'Número de bits inválido. Escolha entre 8, 16 ou 32:'
+        mostrar_erro(erro, linha, stdscr)
+        return escolher_bits(stdscr)
     return bits
     # Lê a quantidade de bits do usuário
 
 def ler_operandos_decimal(stdscr):
 
+    linha = 6
+    stdscr.addstr(0, 0, "*--**--Calculadora Binária--**--*")
+
     # Pede dois números decimais e o operador para o usuário e retorna
-    stdscr.addstr(7, 0, "Digite o primeiro número: ")
+    stdscr.addstr(linha, 2, "Digite o primeiro número: ")
     curses.echo()
-    num1 = stdscr.getstr(7, 27, 20).decode("utf-8")
+    num1 = stdscr.getstr(linha, len("Digite o primeiro número: ")+2, 20).decode("utf-8")
     #tenho que tratar se o numero cabe na quantidade de bits que foi informado
 
-    stdscr.addstr(8, 0, "Digite o operador (+, -, *, /): ")
-    op = stdscr.getstr(8, 35, 1).decode("utf-8")
+    stdscr.addstr(linha+1, 2, "Digite o operador (+, -, *, /): ")
+    op = stdscr.getstr(linha+1, len("Digite o operador (+, -, *, /): ")+2, 1).decode("utf-8")
     #aqui eu tenho que limitar ao total de 1 operador
 
-    stdscr.addstr(9, 0, "Digite o segundo número: ")
-    num2 = stdscr.getstr(9, 27, 20).decode("utf-8")
+    stdscr.addstr(linha+2, 2, "Digite o segundo número: ")
+    num2 = stdscr.getstr(linha+2, len("Digite o segundo número: ")+2, 20).decode("utf-8")
     #tenho que tratar se o numero cabe na quantidade de bits que foi informado
 
     return num1, op, num2
-
-def decimal_para_binario(num1, num2, quantidade_bits):
-    # Converte número decimal para binário em complemento de 2
-    pass
 
 def binario_para_decimal(binario):
     # Converte binário (em lista/vetor) de volta para decimal
@@ -105,11 +124,6 @@ def pressione_tecla(stdscr):
     except ValueError:
         erro = 'Tecla inválida'
         mostrar_erro(erro, stdscr)
-
-def mostrar_erro(erro, stdscr):
-    stdscr.addstr(11, 0, f"Erro: {erro}")
-    stdscr.refresh()
-    pressione_tecla(stdscr)
 
 def adicionar(num1, num2, quantidade_bits, stdscr):
 
